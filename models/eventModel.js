71aa44da0421d10f3f3218667cf01fc8f36b2383
Ketan Kumavat <ketan.kumavat1984@gmail.com>
2024-06-06 14:31:04 +0530

    slight changes in event date

--- models/eventModel.js
+++ models/eventModel.js
@@ -16,7 +16,7 @@ const eventSchema = new mongoose.Schema({
   slug: { type: String, required: true, unique: true },
   title: { type: String, required: true },
   description: { type: String, required: true },
-  date: { type: Date, required: true },
+  date: { type: Date, default: "Coming Soon" },
   time: { type: String, required: true },
   type: { type: String, required: true },
   platform: { type: String },
